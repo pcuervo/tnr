4fe35a79ba136f41b55bb0818aaa07abdaf9bd33
Nayeli Jordan <raul@zamacona.me>
2016-04-18 10:55:36 -0500

    texto y flecha de video

--- sections/parallax_one_logos_section.php
+++ sections/parallax_one_logos_section.php
@@ -3,8 +3,12 @@
 ============================== -->
 <?php
 	echo '<div class="container">';
+		echo '<div class="[ title-video ]">';
+			echo '<h3>PUERTAS INDUSTRIALES DE ALTO RENDIMIENTO</h3>';
+			echo '<a href="http://pcuervo.com/tnr/#our_services_wrap"><img class="[ icon-arrow ]" src="wp-content/themes/Parallax-One/icons/arrow-down-9.png" alt="Ícono flecha abajo"></a>';
+		echo '</div>';
 		/*logos en service section*/
-		echo '<div class="[ services-wrap padding-top--50 ]">';
+		echo '<div class="[ services-wrap ]">';
 			echo '<a href="http://pcuervo.com/tnr/#footer"><button class="standard-button">Contáctanos</button></a>';
 		echo '</div>';
 	echo '</div>';
