817f6e2a8f5797a3d754dc4693e5d0956bb74174
Nayeli Jordan <raul@zamacona.me>
2016-04-18 13:24:04 -0500

    buttom-scroll para boton de contacto en móvil

--- sections/parallax_one_logos_section.php
+++ sections/parallax_one_logos_section.php
@@ -1,4 +1,5 @@
 <!-- =========================
+ Logos en service section
  SECTION: Button header
 ============================== -->
 <?php
@@ -7,7 +8,6 @@
 			echo '<h3>PUERTAS INDUSTRIALES DE ALTO RENDIMIENTO</h3>';
 			echo '<a href="http://pcuervo.com/tnr/#services"><img class="[ icon-arrow ]" src="wp-content/themes/Parallax-One/icons/arrow-down-9.png" alt="Ícono flecha abajo"></a>';
 		echo '</div>';
-		/*logos en service section*/
 		echo '<div class="[ services-wrap ][ bottom--35 ]">';
 			echo '<h3 class="[ colored-text ][ pull-left padding-bottom--20 ]">Gran calidad en nuestras puertas</h3>';
 			echo '<a href="http://pcuervo.com/tnr/#footer" class="[ pull-right ]"><button class="standard-button">Contáctanos</button></a>';
